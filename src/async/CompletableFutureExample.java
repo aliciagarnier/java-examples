744c6168588b91eac3bd988f0d792631960dd99c
alicia <aliciabatistafranco@gmail.com>
2025-02-16 14:39:50 -0300

    reviewing streams

--- src/async/CompletableFutureExample.java
+++ src/async/CompletableFutureExample.java
@@ -6,10 +6,17 @@ import java.util.concurrent.ExecutionException;
 public class CompletableFutureExample {
 
     public void run() throws ExecutionException, InterruptedException {
-        CompletableFuture.runAsync(() -> {
+        CompletableFuture.supplyAsync(() ->  {
             PokemonAbilityFetch pokemonAbilityFetch = new PokemonAbilityFetch("pikachu");
-            pokemonAbilityFetch.fetch();
-        }).get(); // executar essa parte do código onde o resultado (retorno) não importa. desejo apenas executar.
+            return pokemonAbilityFetch.fetch();
+        }).thenAccept((result) -> { // THENRUN
+            System.out.println("Task completed: " + result);
+        }).get();
     }
+//        CompletableFuture.runAsync(() -> {
+//            PokemonAbilityFetch pokemonAbilityFetch = new PokemonAbilityFetch("pikachu");
+//            pokemonAbilityFetch.fetch();
+//        }).get(); // executar essa parte do código onde o resultado (retorno) não importa. desejo apenas executar.
+//    }
 
 }
--- src/streams/streams.java
+++ src/streams/streams.java
@@ -1,32 +1,72 @@
 package streams;
 
-import java.util.ArrayList;
+// O conceito de streams é introduzido no Java 8
+// With Collection interface has two methods to generate a Stream: stream() and
+// parallelStream().
+
+
+// Stream operations are either intermediate or terminal, intermediate operations return a Stream
+// so multiple intermediate operations can be chained before the Stream is closed.
+
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class streams {
 
-    // A API de streams.streams foi introduzida no Java 8, seu objetivo é facilitar trabalhar com conjunto de dados. A ideia é
-    // iterar sobre coleções de objetos, e a cada elemento, realizar alguma ação ou aplicar uma função.
-
     public static void main(String[] args) {
+        Stream<String> fruitStream = Stream.of("apple", "banana", "pear", "kiwi", "orange");
 
-        List<String> frutas = new ArrayList<String>();
+        fruitStream.filter (s -> s.contains("a")) // filter operation retains only elements that match a given predicate
+                .map(s -> s.toUpperCase()) // Using lambda. operation transforms each element using a given function, called a mapper
+                .sorted() // sorts the elements of the Stream according to their natural ordering
+                .forEach(System.out::println); // Method reference. .forEach performs an action which acts on each element of the stream
+        // passing it to a consumer. This is a terminal operation
 
-        frutas.add("maça");
-        frutas.add("banana");
-        frutas.add("mango");
+        System.out.println(Stream.of("apple", "banana", "pear", "kiwi", "orange")
+                .filter(s -> s.contains("a"))
+                .collect(Collectors.toList())); // Collecting elements from a Stream. It's possible to use Collectors.toSet();
 
-        // Esse loop especifica como queremos adicionar o valor de cada elemento. Essa técnica é conhecida como iteração externa (porque é você que especifica como iterar não a biblioteca)
-        // e exige acessar os elementos sequencialmente do início ao fim em uma única thread de execução.
-        // A iteração externa é propensa a erros.
 
-        for (int i = 0; i < frutas.size(); i++)
-        {
-            System.out.println("Fruta: " + frutas.get(i));
-        }
 
-        frutas.stream().forEach(System.out::println); // Não faz nada além de chamar um método
-        //frutas.stream().forEach(item -> System.out.println(item));
+
+        // Note that operations defined on the stream are performed because of the terminal operation. Without a terminal operation
+        // the stream is not processed. Streams cannot be reused. The stream object becomes unusable.
+
+
+        // A Stream object's processing can be sequential or parallel.
+        // In a sequential mode, the elements are processed in the order of the source of the Stream. If the stream, is ordered such as
+        // a SortedMap the processing is guaranteed to make the ordering of the source.
+
+        List<Integer> integerList = Arrays.asList(0,1,2,6,54,3, 7);
+        int howManyOddNumbers = (int) integerList.parallelStream().filter(i -> (i % 2) != 0).count();
+        // integerList.stream().filter.count();
+        System.out.println(howManyOddNumbers);
+
+        // Parallel mode allows the use of multiple threads cores but there is no guarantee of the order in which
+        // elements are processed. If multiple methods are called on a sequential Stream not every method has to be invoked.
+
+
+
+
+        // The groupingBy collector allows the collection of Stream elements into a Map by classifying each element in a group and
+        // performing a downstream operation on the elements classified in the same group.
+
+        Stream.of("apple", "banana", "pear", "kiwi", "orange", "apple", "pear")
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())) // a second collector
+                .entrySet()
+                .forEach(System.out::println);
+
+
 
     }
+
+
+
+
+
+
 }
